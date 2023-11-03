07f9779da45798e59dc98240927bbdd56492091b
rhCat <90529833+rhCat@users.noreply.github.com>
2023-11-02 22:24:11 -0400

    feat: removed finetune from config

--- src/app_utils/gpt_utils.py
+++ src/app_utils/gpt_utils.py
@@ -10,10 +10,7 @@ config = configparser.ConfigParser()
 config.read(os.path.join(config_dir, 'gpt_local_config.cfg'))
 
 openai.api_key = config.get('token', 'GPT_TOKEN')
-fine_tune_model = config.get('model', 'model_for_fine_tune')
-fine_tune_model_id = config.get('model', 'fine_tune_model_id')
 model_for_chat = config.get('model', 'model_for_chat')
-praperation_file = config.get('tools', 'data_praperation_script')
 
 
 # https://github.com/openai/openai-cookbook/blob/main/examples/How_to_count_tokens_with_tiktoken.ipynb
